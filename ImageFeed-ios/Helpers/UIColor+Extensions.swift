067d416143b06e17aa8987de27e0083f6fa4b2a1
Aleksei <ancientt1k1@gmail.com>
2023-07-04 10:05:34 +0300

    Добавлены кастомные цвета. Переименован ViewController

---
+++ ImageFeed-ios/Helpers/UIColor+Extensions.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+Extensions.swift
+//  ImageFeed-ios
+//
+//  Created by Aleksey Kolesnikov on 04.07.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var ypBackground: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+    static var ypBlack: UIColor { UIColor(named: "YP Black") ?? UIColor.black }
+    static var ypBlue: UIColor { UIColor(named: "YP Blue") ?? UIColor.blue }
+    static var ypGray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+    static var ypRed: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+    static var ypWhiteAlpha: UIColor { UIColor(named: "YP White (Alpha 50)") ?? UIColor.white }
+    static var ypWhite: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+    
+}
+
