07071378ef45c7f6ee9bcb309590c274bb3f70de
Aleksei <ancientt1k1@gmail.com>
2023-07-25 14:48:43 +0300

    Сверстан экран профиля. Добавлены autlets и action

--- ImageFeed-ios/Profile/ProfileViewController.swift
+++ ImageFeed-ios/Profile/ProfileViewController.swift
@@ -5,4 +5,18 @@
 //  Created by Aleksey Kolesnikov on 25.07.2023.
 //
 
-import Foundation
+import UIKit
+
+final class ProfileViewController: UIViewController {
+    //MARK: - Outltes
+    @IBOutlet weak var avatarImageview: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var loginNameLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    @IBOutlet weak var logOutButton: UIButton!
+    
+    //MARK: - Actions
+    @IBAction func didTapLogOut() {
+    }
+}
