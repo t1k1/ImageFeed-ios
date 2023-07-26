ff0912bed81ffe2c5d85376a0e2ac33dfdc287b3
Aleksei <ancientt1k1@gmail.com>
2023-07-26 16:22:11 +0300

    Исправлен баг с градиентом, раньше он накладывался при переиспользовании ячеек таблицы. Добавен ScrollView. Добавлена кнопка скачивания. Изображение теперь можно растягивать

--- ImageFeed-ios/SingleImage/SingleImageViewController.swift
+++ ImageFeed-ios/SingleImage/SingleImageViewController.swift
@@ -11,6 +11,7 @@ final class SingleImageViewController: UIViewController {
     //MARK: - Outltes
     @IBOutlet weak var singleImageView: UIImageView!
     @IBOutlet private weak var backButton: UIButton!
+    @IBOutlet private weak var scrollView: UIScrollView!
     
     //MARK: - Actions
     @IBAction private func didTapBackButton() {
@@ -30,5 +31,16 @@ final class SingleImageViewController: UIViewController {
         super.viewDidLoad()
         
         singleImageView.image = image
+        
+        scrollView.minimumZoomScale = 0.1
+        scrollView.maximumZoomScale = 1.25
     }
+}
+
+//MARK: - UIScrollViewDelegate
+extension SingleImageViewController: UIScrollViewDelegate {
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        singleImageView
+    }
+    
 }
