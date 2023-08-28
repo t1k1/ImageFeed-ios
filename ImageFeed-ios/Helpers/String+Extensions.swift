cb6ccd633b6c2b62db49384361c0099b73584285
Aleksei <ancientt1k1@gmail.com>
2023-08-28 15:00:24 +0300

    Исправлено получение картинок для ленты в ImageListService

---
+++ ImageFeed-ios/Helpers/String+Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  String+Extencons.swift
+//  ImageFeed-ios
+//
+//  Created by Aleksey Kolesnikov on 28.08.2023.
+//
+
+import Foundation
+
+extension String {
+    var dateTimeString: Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ssZ"
+        return dateFormatter.date(from: self)
+    }
+}
